01aee8a27ed707fbfd6c11d3bb9715288e5b6835
lachgar <lachgar.m@gmail.com>
2025-01-22 22:08:22 +0100

    'all'

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,23 +1,23 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import Link from "next/link";
+import { useState } from 'react';
+import Link from 'next/link';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleLinkClick = () => {
-    setIsMenuOpen(false); // Close the menu when a link is clicked
+    setIsMenuOpen(false);
   };
 
   return (
-    <header className="bg-blue-600 text-white shadow-md sticky top-0 z-50">
+    <header className="bg-blue-800 text-white shadow-md sticky top-0 z-50">
       <nav className="container mx-auto px-6 py-4 flex justify-between items-center">
         {/* Logo */}
         <div>
           <Link
             href="/"
-            className="text-xl sm:text-2xl lg:text-3xl font-extrabold tracking-wide hover:text-blue-300 transition duration-300"
+            className="text-2xl font-extrabold tracking-wide hover:text-blue-300 transition duration-300"
             onClick={handleLinkClick}
           >
             Dép. Informatique
@@ -27,30 +27,29 @@ const Header = () => {
         {/* Mobile Menu Button */}
         <button
           className={`block md:hidden text-2xl font-semibold focus:outline-none transition duration-300 ${
-            isMenuOpen ? "text-blue-300" : "hover:text-blue-300"
+            isMenuOpen ? 'text-blue-300' : 'hover:text-blue-300'
           }`}
           onClick={() => setIsMenuOpen(!isMenuOpen)}
           aria-label="Toggle Menu"
         >
-          {isMenuOpen ? "✖" : "☰"}
+          {isMenuOpen ? '✖' : '☰'}
         </button>
 
         {/* Navigation Links */}
         <ul
           className={`${
-            isMenuOpen ? "block" : "hidden"
-          } md:flex space-y-2 md:space-y-0 md:space-x-6 bg-blue-700 md:bg-transparent absolute md:static top-16 left-0 w-full md:w-auto text-center md:text-left transition-all duration-300`}
+            isMenuOpen ? 'block' : 'hidden'
+          } md:flex md:items-center md:space-x-6 bg-blue-700 md:bg-transparent absolute md:static top-16 left-0 w-full md:w-auto text-center md:text-left transition-all duration-300`}
         >
-          {[
-            { href: "/presentation", label: "Présentation" },
-            { href: "/corps-enseignant", label: "Corps enseignant" },
-            { href: "/licence", label: "Licence" },
-            { href: "/master", label: "Master" },
+          {[{ href: '/presentation', label: 'Présentation' },
+            { href: '/corps-enseignant', label: 'Corps enseignant' },
+            { href: '/licence', label: 'Licence' },
+            { href: '/master', label: 'Master' },
           ].map((link) => (
             <li key={link.href}>
               <Link
                 href={link.href}
-                className="block text-base sm:text-lg lg:text-xl font-medium hover:text-blue-300 transition duration-300 px-4 py-2 md:p-0"
+                className="block text-lg font-medium hover:text-blue-300 transition duration-300 px-4 py-2 md:p-0"
                 onClick={handleLinkClick}
               >
                 {link.label}
